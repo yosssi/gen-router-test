111ce81315dd41e456dc82bf2566c8cc0bedd8fa
yosssi <yoshida.keiji.84@gmail.com>
2015-01-04 01:10:16 +0900

    Add router.go

--- main.go
+++ main.go
@@ -9,6 +9,18 @@ func topHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "topHandler")
 }
 
+func usersIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "usersIndexHandler")
+}
+
+func usersShowHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "usersShowHandler")
+}
+
+func usersCreateHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "usersCreateHandler")
+}
+
 //go:generate gen-router -i routes.json -o router.go
 func main() {
 	http.Handle("/", newRouter())
