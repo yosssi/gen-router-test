fd013cbdb025e199ac8e68968f9d91358f7b149b
yosssi <yoshida.keiji.84@gmail.com>
2015-01-04 01:07:12 +0900

    Delete cmd/gen-router/router.go

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ func topHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "topHandler")
 }
 
+//go:generate gen-router -i routes.json -o router.go
 func main() {
 	http.Handle("/", newRouter())
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
--- router.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/gorilla/mux"
-
-func newRouter() *mux.Router {
-	r := mux.NewRouter()
-	r.HandleFunc("/", topHandler).Methods("GET")
-	return r
-}
